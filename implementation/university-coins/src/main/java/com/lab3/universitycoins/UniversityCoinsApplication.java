caa3638f48108cc5dc8fcd50e68e57cf2f561e9c
caio-magnani <49353875+caio-magnani@users.noreply.github.com>
2022-05-07 18:35:39 -0300

    Java Fist Commit

---
+++ implementation/university-coins/src/main/java/com/lab3/universitycoins/UniversityCoinsApplication.java
@@ -0,0 +1,13 @@
+package com.lab3.universitycoins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityCoinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityCoinsApplication.class, args);
+	}
+
+}
---
+++ implementation/university-coins/src/test/java/com/lab3/universitycoins/UniversityCoinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab3.universitycoins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityCoinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
